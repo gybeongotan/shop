a23afabc34a68e66995c37358db0e50cb3e8fa50
gybeongotan <gitadmin@gybeongotan.dev>
2022-02-25 09:58:56 +0800

    Automatically change the MongoDb Url base on Node Environment

--- backend/init-database.js
+++ backend/init-database.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-
+const {NODE_ENV,MONGODB_URI_LOCAL,MONGODB_URI_ONLINE} = process.env
 function ConnecDB() {
   return new Promise((resolve, reject) => {
+    let url =  NODE_ENV === 'production' ?  MONGODB_URI_ONLINE : MONGODB_URI_LOCAL
     mongoose
-      .connect(process.env.MONGODB_URI_LOCAL, {
+      .connect(url, {
         useFindAndModify: false,
         useNewUrlParser: true,
         useUnifiedTopology: true,
